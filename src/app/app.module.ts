64495fb1738f3918cfbfc6d943ea2ba12f960dad
kamal <hossaink.5021@gmail.com>
2018-07-20 17:20:45 -0700

    TCS POST

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpModule} from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostComponent
+  ],
+  imports: [
+    HttpModule,
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
